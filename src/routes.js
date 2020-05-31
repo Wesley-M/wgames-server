49aaae6173326159d3f67213c506e4e83387b070
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2020-05-31 09:33:03 -0300

    Updating the alternative route - to feel more natural

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ routes.post('/scores', ScoreController.create);
 routes.get('/questions/:gameId', QuestionController.getQuestionsByGame);
 routes.post('/questions', QuestionController.create);
 
-routes.get('/alternatives/:questionId', AlternativeController.getAlternativesByQuestion);
-routes.post('/alternatives', AlternativeController.create);
+routes.get('/questions/:questionId/alternatives', AlternativeController.getAlternativesByQuestion);
+routes.post('/questions/alternatives', AlternativeController.create);
 
 module.exports = routes;
